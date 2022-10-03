e7778413875e3e818b1d032f4d05c5cbfc282e48
kabzonsihe5 <volchkov_1984@bk.ru>
2022-10-03 22:01:06 +0300

    Project loading

---
+++ src/main/java/ru/volchkov/telegramBot/TelegramPeopleApplication.java
@@ -0,0 +1,12 @@
+package ru.volchkov.telegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramPeopleApplication.class, args);
+	}
+}
