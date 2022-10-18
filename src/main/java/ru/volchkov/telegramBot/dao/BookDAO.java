09d67fbb4b12d67bcacdcae406b95f30707f683e
kabzonsihe5 <volchkov_1984@bk.ru>
2022-10-19 02:58:19 +0300

    added /listOfUsers /takeBook

--- src/main/java/ru/volchkov/telegramBot/dao/BookDAO.java
+++ src/main/java/ru/volchkov/telegramBot/dao/BookDAO.java
@@ -1,4 +1,43 @@
 package ru.volchkov.telegramBot.dao;
 
+import org.springframework.stereotype.Component;
+import ru.volchkov.telegramBot.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class BookDAO {
+
+    List<Book> listOfBooks = new ArrayList<>();
+
+    {
+        listOfBooks.add(newBook("Атлант расправил плечи", "Айн Рэнд", 1957, true));
+        listOfBooks.add(newBook("Человек который смеется", "Виктор Гюго", 1869, true));
+        listOfBooks.add(newBook("Война и мир", "Лев Толстой", 1865, true));
+        listOfBooks.add(newBook("Евгений Онегин", "Александр Пушкин", 1833, true));
+        listOfBooks.add(newBook("Преступление и наказание", "Фёдор Достоевский", 1866, true));
+
+    }
+
+    public List<Book> index() {
+        return listOfBooks;
+    }
+
+    public void addBook(Book book) {
+        listOfBooks.add(book);
+    }
+
+    public Book findBook(String name) {
+        return listOfBooks.stream().filter(a -> a.getName().equals(name)).findAny().orElse(null);
+    }
+
+    public Book newBook(String name, String author, int yearOfRelease, boolean bookStatus) {
+        Book book = new Book();
+        book.setName(name);
+        book.setBookStatus(bookStatus);
+        book.setAuthor(author);
+        book.setYearOfRelease(yearOfRelease);
+        return book;
+    }
 }
--- src/main/java/ru/volchkov/telegramBot/model/Book.java
+++ src/main/java/ru/volchkov/telegramBot/model/Book.java
@@ -1,13 +1,17 @@
 package ru.volchkov.telegramBot.model;
 
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
-@Component
+
+
 @Data
 public class Book {
-    private String Name;
-    private String Author;
-    private String YearOfRelease;
-    private boolean BookStatus;
+    private  String name;
+    private  String author;
+    private  int yearOfRelease;
+    private  boolean bookStatus;
+
+    public boolean getBookStatus(){
+        return bookStatus;
+    }
 }
